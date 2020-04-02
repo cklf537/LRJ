6030a2780cb6ca7e43e1e18e33cb5b152787f2a9
cklf537 <cklf537@gmail.com>
2020-04-02 13:47:20 +0530

    border changes

--- local-remote-jobs/src/App.js
+++ local-remote-jobs/src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
   render(){
   return (
       <Container fluid>
-        <Row>
-          <Col md={2} className='border border-primary'>Logo Section</Col>
-          <Col md={10}> Right Header Seaction</Col>
+        <Row className='border border-primary'>
+          <Col md={2} >Logo Section</Col>
+          <Col md={10} > Right Header Seaction</Col>
         </Row>
         <Row>
         <Col md={2} border={1}>name</Col>
