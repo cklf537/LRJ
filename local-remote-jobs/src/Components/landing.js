337f41e77bf5aeaaf6374f7c5c3926872fffe312
cklf537 <cklf537@gmail.com>
2020-04-10 21:20:40 +0530

    Font colors and the primary button schema css class modified

--- local-remote-jobs/src/Components/landing.js
+++ local-remote-jobs/src/Components/landing.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
-import { Col } from "react-bootstrap";
+import { Col, Card, Row, Button} from "react-bootstrap";
 
-class Landing extends Component{
-    render(){
-        return(
+class Landing extends Component {
+    render() {
+        return (
             <Col>
-                <p>Landing Component</p>
+                <Row>
+                    <Col><Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" />
+  <Card.Body>
+    <Card.Title>Primary Color</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Primary</Button>
+  </Card.Body>
+</Card></Col>
+                    <Col><Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" />
+  <Card.Body>
+    <Card.Title>Secondary Color</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="secondary">Secondary</Button>
+  </Card.Body>
+</Card></Col>
+                    <Col><Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" />
+  <Card.Body>
+    <Card.Title>Success Color</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="success">Success</Button>
+  </Card.Body>
+</Card></Col>
+                    <Col><Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" />
+  <Card.Body>
+    <Card.Title>Warning Color</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="warning">Warning</Button>
+  </Card.Body>
+</Card></Col>
+                </Row>
             </Col>
         );
     }
